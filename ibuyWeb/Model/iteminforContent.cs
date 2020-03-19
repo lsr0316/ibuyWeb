94a95ca322685732f92745d8209eea5ce2397f66
Shirui Liu <i1811398@nzse.ac.nz>
2020-03-20 09:38:43 +1300

    all api design

---
+++ ibuyWeb/Model/iteminforContent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ibuyWeb.Model
+{
+    public class iteminforContent:DbContext
+    {
+
+
+        public iteminforContent(DbContextOptions<iteminforContent> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<iteminfor> iteminfor { get; set; }
+    }
+}
---
+++ ibuyWeb/Model/userinforContent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ibuyWeb.Model
+{
+    public class userinforContent:DbContext
+    {
+
+        public userinforContent(DbContextOptions<userinforContent> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<userinfor> userinfor { get; set; }
+    }
+}
